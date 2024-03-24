e1d7dbe364e49842e2ca5bfa93715c0933c5d682
kq333 <afugowski@yandex.com>
2024-03-24 14:14:29 +0100

    deleted unnecessery code

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,51 +1,44 @@
-// Define the cache name
 const cacheName = 'my-vue-app-cache-v1';
-
-// List of assets to cache
 const assetsToCache = [
   '/',
   '/index.html',
   '/css/app.css',
   '/js/app.js',
   '/addressbook/src/App.vue'
-  // Add paths to other static assets like images, fonts, etc.
 ];
 
-// Install event
 self.addEventListener('install', event => {
-  // Perform installation steps
+
   event.waitUntil(
     caches.open(cacheName)
       .then(cache => {
         console.log('Opened cache');
-        // Cache static assets
+
         return cache.addAll(assetsToCache);
       })
   );
 });
 
-// Fetch event
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return response
+
         if (response) {
           return response;
         }
-        // Clone the request
+
         const fetchRequest = event.request.clone();
 
         return fetch(fetchRequest)
           .then(response => {
-            // Check if we received a valid response
+
             if (!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
-            // Clone the response
+
             const responseToCache = response.clone();
 
-            // Open the cache and add the response
             caches.open(cacheName)
               .then(cache => {
                 cache.put(event.request, responseToCache);
@@ -57,7 +50,6 @@ self.addEventListener('fetch', event => {
   );
 });
 
-// Activate event
 self.addEventListener('activate', event => {
   const cacheWhitelist = [cacheName];
 
@@ -66,7 +58,6 @@ self.addEventListener('activate', event => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
-            // Delete old cache
             return caches.delete(cacheName);
           }
         })
@@ -74,4 +65,3 @@ self.addEventListener('activate', event => {
     })
   );
 });
-////
