2e3393c14ccfcf8113de8d1094d1aabefe82d062
kq333 <afugowski@yandex.com>
2024-03-24 19:05:35 +0100

    added validation to form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,22 +1,42 @@
 <template>
-    <v-card>
-      <v-card-title>{{ dialogTitle }}</v-card-title>
-      <v-card-text>
-        <v-form ref="form">
-          <v-text-field v-model="editedCopy.firstName" label="Imię"></v-text-field>
-          <v-text-field v-model="editedCopy.lastName" label="Nazwisko"></v-text-field>
-          <v-text-field v-model="editedCopy.phone" label="Telefon"></v-text-field>
-          <v-text-field v-model="editedCopy.email" label="Email"></v-text-field>
+  <v-card>
+    <v-card-title>{{ dialogTitle }}</v-card-title>
+    <v-card-text>
+      <v-form ref="form">
+        <v-text-field
+          v-model="editedCopy.firstName"
+          label="Imię"
+          :rules="firstNameRules"
+        >
+        </v-text-field>
+        <v-text-field
+          v-model="editedCopy.lastName"
+          label="Nazwisko"
+          :rules="lastNameRules"
+        >
+        </v-text-field>
+        <v-text-field
+          v-model="editedCopy.phone"
+          label="Telefon"
+          :rules="phoneRules"
+        >
+        </v-text-field>
+        <v-text-field
+          v-model="editedCopy.email"
+          label="Email"
+          :rules="emailRules"
+        >
+        </v-text-field>
 
-        </v-form>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn color="blue darken-1" text @click="closeDialog">Anuluj</v-btn>
-        <v-btn color="blue darken-1" text @click="saveContact">Zapisz</v-btn>
-      </v-card-actions>
-    </v-card>
-  </template>
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="blue darken-1" text @click="closeDialog">Anuluj</v-btn>
+      <v-btn color="blue darken-1" text @click="saveContact">Zapisz</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
 
 <script>
 export default {
@@ -26,7 +46,28 @@ export default {
   },
   data() {
     return {
-      editedCopy: { ...this.editedItem }
+      editedCopy: { ...this.editedItem },
+      firstNameRules: [
+        value => {
+          if (value?.length > 3) return true
+          return 'Imię musi zawierć min. 3 znaki.'
+        },
+      ],
+      lastNameRules: [
+        value => {
+          if (value?.length > 3) return true
+          return 'Nazwisko musi zawierć min. 3 znaki.'
+        },
+      ],
+      phoneRules: [
+        value => !!value || 'Numer tel. musi zawirać min. 9 cyfr.',
+        value => /^[0-9]{9}$/.test(value) || 'Numer tel. musi zawirać min. 9 cyfr.',
+      ],
+
+      emailRules: [
+        v => !!v || 'E-mail musi być podany',
+        v => /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()\\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v) || 'E-mail musi być poprawny',
+      ],
     };
   },
 
@@ -48,8 +89,14 @@ export default {
       }
     },
     saveContact() {
-      this.$emit('save-contact', this.editedCopy);
-      this.closeDialog();
+
+      const isValid = this.$refs.form.validate();
+      if (isValid) {
+        this.$emit('save-contact', this.editedCopy);
+        this.closeDialog();
+      } else {
+        console.log("Błąd Formularza.");
+      }
     }
   },
 };
